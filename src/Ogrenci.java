dc196432ff630c7363bab58378a565c9fcd2c624
atessed <sedaates@gmail.com>
2022-07-07 18:40:11 +0300

    ikinci değişiklik yapıldı

--- src/Ogrenci.java
+++ src/Ogrenci.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Ogrenci {
+public class Ogrenci {
+    public static void main(String[] args) {
+        System.out.println("merhaba dünya");
+        System.out.println("merhaba dünya 2");
+    }
 }
